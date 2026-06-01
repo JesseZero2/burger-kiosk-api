eb042ed35c8093586e84f99fdfb711e62faa07f9
jek <jakebernal512@gmail.com>
2026-06-01 17:59:12 +0800

    ready to test api

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,10 @@ Route::put('/orders/{order}/status', [OrderController::class, 'updateStatus']);
 Route::middleware(ApiKeyMiddleware::class)->group(function () {
 
     Route::get('/menu-items', [MenuItemController::class, 'index']);
+    Route::get('/menu-items/{id}', [MenuItemController::class, 'show']);
 
     Route::post('/menu-items', [MenuItemController::class, 'store']);
-
     Route::put('/menu-items/{id}', [MenuItemController::class, 'update']);
-
     Route::delete('/menu-items/{id}', [MenuItemController::class, 'destroy']);
 
 });
