c75c176eaf254260cc0f941518b5eb3c05265a8c
JesseZero2 <jessiemaralit00@gmail.com>
2026-06-04 04:53:28 +0800

    Serve uploaded menu item images

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/storage/menu-items/{filename}', function ($filename) {
+    $path = 'menu-items/' . $filename;
+
+    if (!Storage::disk('public')->exists($path)) {
+        abort(404);
+    }
+
+    return response()->file(Storage::disk('public')->path($path));
 });
